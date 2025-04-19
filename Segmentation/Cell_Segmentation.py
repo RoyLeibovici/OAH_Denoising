e54df2971c43ef0d98c90fd22b697b49e23b3ff8
Roy Leibovici <roy.leibovici@gmail.com>
2025-04-19 14:10:04 +0300

    segmentation

--- Segmentation/Cell_Segmentation.py
+++ Segmentation/Cell_Segmentation.py
@@ -1,1 +1,6 @@
 import numpy as np
+from cellSAM import segment_cellular_image
+
+path = r'G:\My Drive\Shlomi and Roy\Final Project\code\frames\frame_0004.png'
+img = np.load(path)
+mask, _, _ = segment_cellular_image(img, device='cuda')
