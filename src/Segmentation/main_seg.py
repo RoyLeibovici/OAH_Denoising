f50e6ae4dc0ebea7db227999d3f58e6e1d44591f
Roy Leibovici <roy.leibovici@gmail.com>
2025-05-24 10:50:53 +0300

    autoencoder creation

--- src/Segmentation/main_seg.py
+++ src/Segmentation/main_seg.py
@@ -11,12 +11,10 @@ Data_path = Path(project_path, "Data")
 Videos_path = Path(Data_path, "Input_Videos")
 Output_files = Path(Data_path, "Output_files")
 
-video_path = Path(Videos_path, 'HTB5-170122.AVI')
+for video_path in Videos_path.glob("*.AVI"):
+    video_outputs, frames_path = sample_frames(video_path, Output_files, sampling_rate=1)
+    # segment_and_sort_frames(frames_path, video_outputs)
 
-
-video_outputs, frames_path = sample_frames(video_path, Output_files, sampling_rate=1)
-# segment_and_sort_frames(frames_path, video_outputs)
-
-frames_with_masks_path = Path(video_outputs, "frames with mask")
-masks_path = Path(video_outputs, "masks")
-crop_cells(frames_with_masks_path, masks_path, video_outputs)
+    frames_with_masks_path = Path(video_outputs, "frames with mask")
+    masks_path = Path(video_outputs, "masks")
+    crop_cells(frames_with_masks_path, masks_path, video_outputs)
